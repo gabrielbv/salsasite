9523c3301f1f30660bd44daf3c7c236e5afa0183
stres <stres@stres-Aspire-7730ZG.(none)>
2013-05-28 15:58:54 +0300

    registration update

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -11,6 +11,35 @@ from django.contrib.auth.models import User
 
 
 class AccountTest(TestCase):
+    def test_failed_registrations(self):
+    
+        url = reverse("register")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        data = {
+            "username":"foo",
+            "firstname":"foo",
+            "lastname":"foo",
+            "email":"foo@bar.com",
+            "confirm_email":"foobar@bar.com",
+            "password":"foo",
+            "confirm_password":"foo",
+            "country":"foo",
+        }
+        response = self.client.post(url, data,)
+        
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(User.objects.filter(username=data["username"]).count(), 0)
+
+        data['confirm_email'] = data['email']
+        
+        data['password'] = "foobar"
+        response = self.client.post(url, data,)
+        self.assertEqual(User.objects.filter(username=data["username"]).count(), 0)
+        
+
     def test_registration(self):
     
     	url = reverse("register")
@@ -21,8 +50,8 @@ class AccountTest(TestCase):
             "username":"foo",
             "firstname":"foo",
             "lastname":"foo",
-            "email":"foo",
-            "confirm_email":"foo",
+            "email":"foo@bar.com",
+            "confirm_email":"foo@bar.com",
             "password":"foo",
             "confirm_password":"foo",
             "country":"foo",
@@ -31,6 +60,7 @@ class AccountTest(TestCase):
         
         self.assertEqual(response.status_code, 302)
 
-        self.assertEqual(User.objects.filter(username=data["username"]).count(),1)
+        self.assertEqual(User.objects.filter(username=data["username"]).count(), 1)
 
-
+        logged_in = self.client.login(username=data["username"], password=data["password"])
+        self.assertTrue(logged_in)
