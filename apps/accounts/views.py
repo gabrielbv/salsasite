0c775d098adaa866235c90ca54fd10e0d2b4240b
jitarulucian@yahoo.com <jitarulucian@yahoo.com>
2013-06-03 10:57:03 +0300

    user profiles finis

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -63,3 +63,15 @@ def view_profile(request):
 
 
    
+#def view_music_manage(request):
+
+#    profile = UserProfile.objects.get(user_id = request.user.id)
+
+
+#    return render(request, 'accounts/profile.html', {
+    
+#            'first_name': request.user.first_name,
+#            'last_name': request.user.last_name,
+#            "profile" : profile,
+    
+#    })
