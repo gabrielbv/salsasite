2d92bfd5a0d0569bf796558d300bf892a67575d5
jitarulucian@yahoo.com <jitarulucian@yahoo.com>
2013-06-12 15:04:20 +0300

    sjldhkfjs

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,17 @@
+window.Song = Backbone.Model.extend();
+
+window.SongCollection = Backbone.Collection.extend({
+    model:Song,
+    url:"../api/songs"
+});
+
+
 window.SongListView = Backbone.View.extend({
  
     tagName:'ul',
  
     initialize:function () {
-        this.model.bind("reset", this.render, this);
+        this.model.fetch("reset", this.render, this);
     },
  
     render:function (eventName) {
@@ -38,3 +46,28 @@ window.SongView = Backbone.View.extend({
     }
  
 });
+
+var AppRouter = Backbone.Router.extend({
+
+    routes:{
+        "":"list",
+        "song/:id":"songDetails"
+    },
+
+    list:function () {
+        this.SongList = new SongCollection();
+        this.SongListView = new SongListView({model:this.songList});
+        this.SongList.fetch();
+        $('#sidebar').html(this.SongListView.render().el);
+    },
+
+    wineDetails:function (id) {
+        this.song = this.SongList.get(id);
+        this.SongView = new SongView({model:this.song});
+        $('#content').html(this.SongView.render().el);
+    }
+});
+
+var app = new AppRouter();
+/*Backbone.history.start();*/
+Backbone.history.start({pushState: true});
