7960b74b65e843b25b8fe850cabbf5b9910713c9
gabi <gabi@gabi-F5SL.(none)>
2013-06-17 10:55:44 +0300

    backbone-django

--- apps/songs/api/resources.py
+++ apps/songs/api/resources.py
@@ -2,7 +2,12 @@ from tastypie.resources import ModelResource
 from songs.models import Song
 
 class SongResource(ModelResource):
+
+    def determine_format(self,request):
+        return 'application/json'
     class Meta:
         queryset = Song.objects.all()
         allowed_methods = ['get']
+
+
 
