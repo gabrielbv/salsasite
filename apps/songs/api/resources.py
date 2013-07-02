d688622d008490575506c962382e8f05424e6cbe
vagrant <vagrant@vagrant.vm>
2013-07-02 09:22:56 +0000

    modify resources

--- apps/songs/api/resources.py
+++ apps/songs/api/resources.py
@@ -1,11 +1,21 @@
 from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
+from tastypie import fields	
 
 
 from songs.models import Song
+from accounts.models import UserProfile
+
+
+class UserResource(ModelResource):
+	class Meta:
+	    queryset = UserProfile.objects.all()
+        resource_name = 'user'
 
 class SongResource(ModelResource):
+
+    user = fields.ForeignKey(UserResource, 'user')
 
     def determine_format(self,request):
         return 'application/json'
