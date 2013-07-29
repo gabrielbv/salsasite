dab604574315bf2a3315dcc580ad604f350a69ea
gabi <gabi@gabi-F5SL.(none)>
2013-07-29 17:04:44 +0300

    minor changes

--- static/js/songs/views.js
+++ static/js/songs/views.js
@@ -1,10 +1,13 @@
 window.SongListView=Backbone.View.extend({
+
     tagName:'div',
     template:_.template($("#tpl-song-list").html()),
 
     render:function(){
-    this.$el.html(this.template(this.model.toJson));
-    return this;
+
+        this.$el.html(this.template(this.model.toJson));
+        
+        return this;
 
     }
 });
