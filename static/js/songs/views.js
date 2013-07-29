f2bbc0adc476a05befca431006e17c6759f030d8
gabi <gabi@gabi-F5SL.(none)>
2013-07-29 16:55:19 +0300

    minor changes

--- static/js/songs/views.js
+++ static/js/songs/views.js
@@ -2,14 +2,8 @@ window.SongListView=Backbone.View.extend({
     tagName:'div',
     template:_.template($("#tpl-song-list").html()),
 
-
-    initialize:function(){
-        this.model.bind('reset',this.render,this)
-
-    },
-
     render:function(){
-    this.$el.html(this.template(this.model.attributes));
+    this.$el.html(this.template(this.model.toJson));
     return this;
 
     }
