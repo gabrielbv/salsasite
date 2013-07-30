3d3ee3bfeab25d6d12d272ccccc5904e349e725f
gabi <gabi@gabi-F5SL.(none)>
2013-07-30 23:01:46 +0300

    minor changes

--- static/js/songs/views.js
+++ static/js/songs/views.js
@@ -3,15 +3,10 @@ window.SongListView=Backbone.View.extend({
     tagName:'div',
     template:_.template($("#tpl-song-list").html()),
 
-    initialize:function(){
-        this.collection.bind('reset',this.render,this);
-
-    }
-
 
     render:function(eventName){
-        _.each(this.model.models,function(song){
-            $(this.el).append(new SongListItemView({collection:song}).render().el);
+        _.each(this.collection.models,function(){
+            $(this.el).append(this.collection({collection:song}).render().el);
         },this);
         return this;
     }
@@ -20,12 +15,3 @@ window.SongListView=Backbone.View.extend({
     
 });
 
-window.SongListItemView=Backbone.View.extend({
-    tagName:'div'
-    template:_.template($('tpl-song-item').html()),
-
-    render:function(eventName){
-        $(this.el).html(this.template(this.model.toJSON()));
-        return this;
-    }
-})
