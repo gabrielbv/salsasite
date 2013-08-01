8d16679b00579b0a78d49bc9b42410831cfeef6a
gabi <gabi@gabi-F5SL.(none)>
2013-08-01 11:10:03 +0300

    changes to event details

--- static/js/events/router.js
+++ static/js/events/router.js
@@ -8,20 +8,15 @@ Salsasite.AppRouter = Backbone.Router.extend({
     list:function(){
 
         this.eventListView = new Salsasite.EventListView({collection:Salsasite.events})
-        
+      
         $('#content').html(this.eventListView.render().el)
     },
 
     event_details:function(id){
+        this.event=Salsasite.events.get(id)
 
-        this.eventListView = new Salsasite.EventListView({collection:Salsasite.events})
-        
-        this.event=this.eventListView.toJSON().id;
-        
-        this.eventDetailsView=new Salsasite.EventDetailsView({collection:Salsasite.events})
-        
-        $('#content').html(this.eventDetailsView.render().el)
+        this.eventDetailsViews= new Salsasite.EventDetailsView({collection:this.event})
+        $('#content').html(this.eventDetailsViews.render().el)
     }
 })
-
 
--- static/js/events/views.js
+++ static/js/events/views.js
@@ -9,8 +9,9 @@ Salsasite.EventListView=Backbone.View.extend({
         var self=this
         
         if(this.collection.toJSON().length >0){
+            console.log(this.collection)
         _.each(this.collection.models,function(event){
-            console.log(event.toJSON().id)
+            
 
             
             $(self.el).append(self.template(event.toJSON()))
@@ -26,16 +27,14 @@ Salsasite.EventListView=Backbone.View.extend({
 });
 
 Salsasite.EventDetailsView=Backbone.View.extend({
-
     tagName:'div',
+
     initialize:function(){
         this.template=_.template($('#tpl-event-details').html());
-
-    }
-
+    },
 
     render:function(event){
-        $(this.el).html(this.template(event.toJSON()))
-
+        $(this.el).html(this.template(this.collection.toJSON()))
+        return this;
     },
-/*})*/
+})
